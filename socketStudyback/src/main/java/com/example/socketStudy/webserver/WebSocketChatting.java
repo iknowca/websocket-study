f19eebfe9d9681f0d86c4cbc647e810e6bb02753
wonki kim <iknowca@gmail.com>
2023-06-21 00:26:50 +0900

    feat: [김원기] 메시지에 session.getId 추가

--- socketStudyback/src/main/java/com/example/socketStudy/webserver/WebSocketChatting.java
+++ socketStudyback/src/main/java/com/example/socketStudy/webserver/WebSocketChatting.java
@@ -9,6 +9,7 @@ import jakarta.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,7 +21,7 @@ public class WebSocketChatting {
     private static Set<Session> CLIENTS = Collections.synchronizedSet(new HashSet<>());
 
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session) throws IOException {
         log.info(session.toString());
         if(CLIENTS.contains(session)) {
             log.info("이미 연결되 세션입니다. > " + session);
@@ -28,12 +29,15 @@ public class WebSocketChatting {
             CLIENTS.add(session);
             log.info("새로운 세션입니다 > " + session);
         }
+        for(Session client: CLIENTS) {
+            client.getBasicRemote().sendText(session.getId()+"님이입장했습니다");
+        }
     }
 
     @OnClose
     public void onClose(Session session) throws Exception {
         CLIENTS.remove(session);
-        log.info("세션을 닫습니다 : " + session);
+        log.info("세션을 닫습니다 : " + session.getId());
     }
 
     @OnMessage
@@ -41,8 +45,8 @@ public class WebSocketChatting {
         log.info("입력된 메시지입니다. > " + message);
 
         for(Session client: CLIENTS) {
-            log.info("메시지를 전달합니다. > " + message);
-            client.getBasicRemote().sendText(message);
+            log.info(session.getId() + "번이 메시지를 전달합니다. > " + message);
+            client.getBasicRemote().sendText(session.getId()+": "+message);
         }
     }
 }
